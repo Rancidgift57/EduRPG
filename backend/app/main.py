d9580526f1c2dc70ed82aefabf63ccb50a218724
NIKHIL NAIR <149157433+Rancidgift57@users.noreply.github.com>
2026-03-17 17:03:02 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -55,13 +55,13 @@ app = FastAPI(
 )
 
 origins = [
-    "https://edu-rpg-six.vercel.app",
+    "https://edu-rpg-six.vercel.app","http://localhost:3000"
 ]
 
 # ── CORS ──────────────────────────────────────────────────────────────
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.CORS_ORIGINS,
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
